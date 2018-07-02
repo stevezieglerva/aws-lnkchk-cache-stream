277c3aa6410c5645f957a056e580738c7e76d88e
stevezieglerva <stephen.v.ziegler@gmail.com>
2018-07-02 09:44:43 -0400

    Updated logging

--- lambda_function.py
+++ lambda_function.py
@@ -9,9 +9,21 @@ def lambda_handler(event, context):
 
 
     es = ESLambdaLog("aws_lnkchk_cache_stream")
+
+    esraw = ESLambdaLog("aws_lnkchk_cache_stream_raw")
+    esraw.log_event(event)
     
     print("Logging to ES")
-    es.log_event(event)
+    for record in event["Records"]:
+        event_name = record["eventName"]
+        url = record["dynamodb"]["Keys"]["url"]["S"]
+        image = "NewImage"
+        if event_name == "REMOVE":
+            image = "OldImage"
+        http_response = record["dynamodb"][image]["http_response"]["S"]
+        print("\t" + event_name + " " + url)
+        es_event = {"event" : event_name, "url" : url, "http_response" : http_response}
+        es.log_event(es_event)
 
     print("Finished.")
  
