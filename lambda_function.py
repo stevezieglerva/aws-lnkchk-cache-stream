dd603764574df52c29524cbb28d5a5cf21b70b8d
stevezieglerva <stephen.v.ziegler@gmail.com>
2018-07-08 14:20:01 -0400

    Log raw data enriched data

--- lambda_function.py
+++ lambda_function.py
@@ -4,17 +4,13 @@ from ESLambdaLog import *
 
 def lambda_handler(event, context):
     print("In lambda_handler " + str(datetime.now()))
-    print("Event:")
-    print(json.dumps(event, indent=True))
-
-
+    
+    esraw = ESLambdaLog("aws_lnkchk_cache_stream_raw")
     es = ESLambdaLog("aws_lnkchk_cache_stream")
 
-    esraw = ESLambdaLog("aws_lnkchk_cache_stream_raw")
-    esraw.log_event(event)
-    
     print("Logging to ES")
     for record in event["Records"]:
+        esraw.log_event(record)
         event_name = record["eventName"]
         url = record["dynamodb"]["Keys"]["url"]["S"]
         image = "NewImage"
@@ -22,8 +18,12 @@ def lambda_handler(event, context):
             image = "OldImage"
         http_response = record["dynamodb"][image]["http_result"]["S"]
         print("\t" + event_name + " " + url)
-        es_event = {"event" : event_name, "url" : url, "http_response" : http_response}
+        link_check = "good"
+        if int(http_response) >= 400:
+            link_check = "bad"
+        es_event = {"event" : event_name, "url" : url, "http_response" : http_response, "link_check" : link_check}
         es.log_event(es_event)
+        print(link_check + " - " + url)
 
     print("Finished.")
  
